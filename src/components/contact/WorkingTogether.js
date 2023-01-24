105332a0c2648078c0211690d8fb0675eeff17bb
Jonas Khoza <jonaskhoza18@gmail.com>
2023-01-24 13:05:41 +0200

    added the remaining code of Contact page

--- src/components/contact/WorkingTogether.js
+++ src/components/contact/WorkingTogether.js
@@ -1,33 +1,71 @@
 import classes from "./styles/working_together.module.css";
 
 function WorkingTogether() {
-  <section className={classes.working_together}>
-    <div className={classes.statement}>
-      <h1>Working together?</h1>
-      <p>
-        We are always open to new products. Especially if they are of good
-        origin, and are made without artifice, we would love to hear from you!
-      </p>
-    </div>
-    <div className={classes.form_container}>
-      <form>
-        <div>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Brand or Company" />
+  return (
+    <section className={classes.working_together}>
+      <div className={classes.statement}>
+        <h1>Working together?</h1>
+        <p>
+          We are always open to new products. Especially if they are of good
+          origin, and are made without artifice, we would love to hear from you!
+        </p>
+      </div>
+      <div className={classes.contact_form_container}>
+        <div className={classes.contact_container}>
+          <form>
+            <div className={classes.inputs}>
+              <div className={classes.inputs_container}>
+                <input
+                  type="text"
+                  name="fullname"
+                  placeholder="Name"
+                  required
+                />
+                <input
+                  type="text"
+                  name="company"
+                  placeholder="Brand or Company"
+                  required
+                />
+              </div>
+              <div className={classes.inputs_container}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="E-mail"
+                  required
+                />
+                <input type="tel" name="phone" placeholder="Phone" required />
+              </div>
+            </div>
+            <div className="textarea">
+              <textarea
+                id="message"
+                name="message"
+                rows="8"
+                placeholder="Your Message"
+                required
+              ></textarea>
+            </div>
+            <div className={classes.form_actions}>
+              {" "}
+              <label>
+                <input
+                  type="checkbox"
+                  name="terms_conditions"
+                  className={classes.terms_conditions}
+                />
+                I accept the terms and conditions
+              </label>
+              <div>
+                <button>Send</button>
+              </div>
+            </div>
+          </form>
         </div>
-        <div>
-          <input type="email" placeholder="E-mail" />
-          <input type="tel" placeholder="Phone" />
-        </div>
-        <div>
-          <textarea rows="8" placeholder="Your Message"></textarea>
-        </div>
-        <div className={classes.form_actions}>
-          <button>Send</button>
-        </div>
-      </form>
-    </div>
-  </section>;
+      </div>
+    </section>
+  );
 }
 
 export default WorkingTogether;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SocialMedia from "../components/contact/SocialMedia";
+import WorkingTogether from "../components/contact/WorkingTogether";
 function Contact() {
   return (
     <React.Fragment>
       <SocialMedia />
+      <WorkingTogether />
     </React.Fragment>
   );
 }
